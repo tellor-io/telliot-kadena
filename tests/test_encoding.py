8cb7e100abb2a5206b3929711eb3a2c62c1a24f8
akrem <akremstudy@gmail.com>
2023-02-21 12:52:40 -0500

    initial commit

---
+++ tests/test_encoding.py
@@ -0,0 +1,15 @@
+from telliot_kadena.utils.encoding import urlsafe_base64_encode_string, urlsafe_base64_decode_string
+
+
+def test_data_encode() -> None:
+    """Tests the urlsafe_base64_encode_string and urlsafe_base64_decode_string functions."""
+    # Test encoding and decoding a simple string
+    encode_hello = urlsafe_base64_encode_string("hello")
+    decode_hello = urlsafe_base64_decode_string(encode_hello)
+    assert "hello" == decode_hello, "urlsafe_base64_encode_string and urlsafe_base64_decode_string should be inverses"
+
+    # Test encoding and decoding a more complex string
+    spec = '{SpotPrice: [kda,usd]}'
+    encode_query_data = urlsafe_base64_encode_string(spec)
+    decode_query_data = urlsafe_base64_decode_string(encode_query_data)
+    assert spec == decode_query_data, "urlsafe_base64_encode_string and urlsafe_base64_decode_string should be inverses"
